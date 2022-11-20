80d532824bd003bec985ac8fcdad1026dee777e8
furkankarakocmobilist <furkan.karakoc@mobilistbilisim.com>
2022-11-21 00:00:43 +0300

    Initial Commit

---
+++ iOS Yasam Dongusu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS Yasam Dongusu
+//
+//  Created by Furkan on 21.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

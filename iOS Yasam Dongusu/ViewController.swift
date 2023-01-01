c3923419642ddf665fe90314029ced3b133264cc
furkankarakocmobilist <furkan.karakoc@mobilistbilisim.com>
2023-01-02 00:04:51 +0300

    first commit

--- iOS Yasam Dongusu/ViewController.swift
+++ iOS Yasam Dongusu/ViewController.swift
@@ -11,9 +11,24 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("viewDidLoad çalıştı")
     }
+    override func viewWillAppear(_ animated: Bool) {
+        print("viewWillAppear çalıştı")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        print("viewDidAppear çalıştı")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        print("viewWillDisappear çalıştı")
 
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        print("viewDidDisappear çalıştı")
 
+    }
 }
 
